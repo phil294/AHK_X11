e679aafce7a44d2da5163fd32b847e15ed1525ce
phil294 <philip@waritschlager.de>
2022-07-03 12:20:39 +0200

    accept script via argv

--- src/ahkx11.cr
+++ src/ahkx11.cr
@@ -1,14 +1,15 @@
 require "./build"
 require "./run"
 
-# test code
-ahk_script = [
-	"Echo some te,,,xt",
-	" 	 filecopy a*,  aout ,  ",
-]
+if ! ARGV[0]?
+	abort "Missing file argument.\nUsage:\n    ahkx11 path/to/script.ahk"
+end
+
+ahkstr = File.read ARGV[0]
+ahkarr = ahkstr.split /\r?\n/
 
 begin
-	start = Builder.new.build ahk_script
+	start = Builder.new.build ahkarr
 rescue e : SyntaxException | ParsingException
 	# TODO msgbox
 	abort e.message
