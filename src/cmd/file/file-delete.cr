b9debdfafdb24f3924c7b06719d1d0ba93965a1c
phil294 <philip@waritschlager.de>
2022-08-19 22:28:39 +0200

    add `FileDelete`

---
+++ src/cmd/file/file-delete.cr
@@ -0,0 +1,17 @@
+require "../base"
+
+class Cmd::File::FileDelete < Cmd::Base
+	def self.min_args; 1 end
+	def self.max_args; 1 end
+	def self.sets_error_level; true end
+	def run(thread, args)
+		begin
+			Dir.glob(args[0]).each do |src|
+				::File.delete src
+			end
+			"0"
+		rescue
+			"1"
+		end
+	end
+end
