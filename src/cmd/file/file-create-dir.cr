4c75e29ece36784bb7324ad1dd1477fe520c7c7f
phil294 <philip@waritschlager.de>
2022-08-19 22:09:29 +0200

    add `FileCreateDir`

---
+++ src/cmd/file/file-create-dir.cr
@@ -0,0 +1,15 @@
+require "../base"
+
+class Cmd::File::FileCreateDir < Cmd::Base
+	def self.min_args; 1 end
+	def self.max_args; 1 end
+	def self.sets_error_level; true end
+	def run(thread, args)
+		begin
+			::Dir.mkdir_p args[0]
+			"0"
+		rescue
+			"1"
+		end
+	end
+end
