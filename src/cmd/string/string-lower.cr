8cb641cc103da6a2c76d8f07a063262f469833ad
phil294 <philip@waritschlager.de>
2022-08-24 02:20:41 +0200

    add `StringLower`, `StringUpper`

---
+++ src/cmd/string/string-lower.cr
@@ -0,0 +1,15 @@
+class Cmd::String::StringLower < Cmd::Base
+	def self.min_args; 2 end
+	def self.max_args; 3 end
+	def run(thread, args)
+		out_var, in_var = args
+		title_case = args[2]? && args[2].downcase == "t"
+		text = thread.runner.get_user_var(in_var)
+		if title_case
+			text = text.titleize
+		else
+			text = text.downcase
+		end
+		thread.runner.set_user_var(out_var, text)
+	end
+end
