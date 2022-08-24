ec8acb9eb77aef7ccea1e275bbefc6e619afe6e8
phil294 <philip@waritschlager.de>
2022-08-24 02:30:40 +0200

    add `StringMid`

---
+++ src/cmd/string/string-mid.cr
@@ -0,0 +1,18 @@
+class Cmd::String::StringMid < Cmd::Base
+	def self.min_args; 4 end
+	def self.max_args; 5 end
+	def run(thread, args)
+		out_var, in_var, start, count = args
+		start = start.to_i? || 1
+		start = 1 if start < 1
+		start -= 1
+		count = count.to_i?
+		if ! count || count < 1
+			text = ""
+		else
+			text = thread.runner.get_user_var(in_var)
+			text = text[start, count]
+		end
+		thread.runner.set_user_var(out_var, text)
+	end
+end
