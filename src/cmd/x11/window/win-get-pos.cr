507dc0d9fb34f98a6c1aaefa75a4daa2b1e31db8
phil294 <philip@waritschlager.de>
2022-08-24 22:27:04 +0200

    add `WinGetPos`

---
+++ src/cmd/x11/window/win-get-pos.cr
@@ -0,0 +1,27 @@
+require "./win-util"
+
+class Cmd::X11::Window::WinGetPos < Cmd::Base
+	def self.min_args; 0 end
+	def self.max_args; 5 end
+	def run(thread, args)
+		match_conditions = args[4..]? || [] of ::String
+		out_x = args[0]?
+		out_y = args[1]?
+		out_w = args[2]?
+		out_h = args[3]?
+		found = Util.match(thread, match_conditions, empty_is_last_found: true, a_is_active: true) do |win|
+			x, y = win.location
+			w, h = win.size
+			thread.runner.set_user_var(out_x, x.to_s) if out_x
+			thread.runner.set_user_var(out_y, y.to_s) if out_y
+			thread.runner.set_user_var(out_w, w.to_s) if out_w
+			thread.runner.set_user_var(out_h, h.to_s) if out_h
+		end
+		if ! found
+			thread.runner.set_user_var(out_x, "") if out_x
+			thread.runner.set_user_var(out_y, "") if out_y
+			thread.runner.set_user_var(out_w, "") if out_w
+			thread.runner.set_user_var(out_h, "") if out_h
+		end
+	end
+end
