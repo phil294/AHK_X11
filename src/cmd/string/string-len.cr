a3a690047568dcf26291920018264237135d75b5
phil294 <philip@waritschlager.de>
2022-08-24 02:15:29 +0200

    add `StringLen`

---
+++ src/cmd/string/string-len.cr
@@ -0,0 +1,9 @@
+class Cmd::String::StringLen < Cmd::Base
+	def self.min_args; 2 end
+	def self.max_args; 2 end
+	def run(thread, args)
+		out_var, in_var = args
+		text = thread.runner.get_user_var(in_var)
+		thread.runner.set_user_var(out_var, text.size.to_s)
+	end
+end
