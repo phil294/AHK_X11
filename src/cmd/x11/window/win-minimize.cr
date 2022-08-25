aa8829e4c2c560c82cbb4b6daae7ffa7a29823e0
phil294 <philip@waritschlager.de>
2022-08-25 04:23:25 +0200

    add `WinMinimize`

---
+++ src/cmd/x11/window/win-minimize.cr
@@ -0,0 +1,11 @@
+require "./win-util"
+
+class Cmd::X11::Window::WinMinimize < Cmd::Base
+	def self.min_args; 0 end
+	def self.max_args; 4 end
+	def run(thread, args)
+		Util.match(thread, args, empty_is_last_found: true, a_is_active: true) do |win|
+			win.minimize!
+		end
+	end
+end
