d71d1ace0e83bbd79aee3fc934e52ff1ca4c6a4c
phil294 <philip@waritschlager.de>
2022-08-26 06:25:04 +0200

    add `IfExist` / `IfNotExist`

---
+++ src/cmd/control-flow/if-exist.cr
@@ -0,0 +1,9 @@
+class Cmd::ControlFlow::IfExist < Cmd::Base
+	def self.min_args; 1 end
+	def self.max_args; 1 end
+	def self.multi_command; true end
+	def self.conditional; true end
+	def run(thread, args)
+		! Dir.glob(args[0]).empty?
+	end
+end
