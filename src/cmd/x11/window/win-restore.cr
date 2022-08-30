d863873ad885197e82faed4ac7553fa01c4461c3
phil294 <philip@waritschlager.de>
2022-08-30 13:09:07 +0200

    add `WinRestore`

---
+++ src/cmd/x11/window/win-restore.cr
@@ -0,0 +1,12 @@
+require "./win-util"
+
+class Cmd::X11::Window::WinRestore < Cmd::Base
+	def self.min_args; 0 end
+	def self.max_args; 4 end
+	def run(thread, args)
+		Util.match(thread, args, empty_is_last_found: true, a_is_active: true) do |win|
+			win.set_state(XDo::StateAction::Remove, "maximized_horz")
+			win.set_state(XDo::StateAction::Remove, "maximized_vert")
+		end
+	end
+end
