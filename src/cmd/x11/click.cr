b5ac744586edd3e99150dfefc71a32cea250acbd
phil294 <philip@waritschlager.de>
2022-08-30 12:45:20 +0200

    add `MouseClick` (needs to be redone)

---
+++ src/cmd/x11/click.cr
@@ -0,0 +1,12 @@
+class Cmd::X11::MouseClick < Cmd::Base
+	def self.min_args; 1 end
+	def self.max_args; 3 end
+	def run(thread, args)
+		# TODO: this is just quickly hacked together, implement proper mouse commands
+		thread.runner.x11.pause do
+			_, _, screen = thread.runner.x_do.mouse_location
+			thread.runner.x_do.move_mouse (args[1]?.try &.to_i? || 0), (args[2]?.try &.to_i? || 0), screen
+			thread.runner.x_do.click XDo::Button::Left
+		end
+	end
+end
