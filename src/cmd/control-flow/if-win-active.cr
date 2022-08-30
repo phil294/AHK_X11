e0fa0357f1fe255bbd5cf08e5b0e0b3d8b7b1867
phil294 <philip@waritschlager.de>
2022-08-30 13:20:50 +0200

    add `IfWinActive` / `IfWinNotActive`

---
+++ src/cmd/control-flow/if-win-active.cr
@@ -0,0 +1,13 @@
+class Cmd::ControlFlow::IfWinActive < Cmd::Base
+	def self.min_args; 0 end
+	def self.max_args; 4 end
+	def self.multi_command; true end
+	def self.conditional; true end
+	def run(thread, args)
+		active = false
+		Cmd::X11::Window::Util.match(thread, args, empty_is_last_found: true, a_is_active: false) do |win|
+			active = win == thread.runner.x_do.active_window
+		end
+		active
+	end
+end
