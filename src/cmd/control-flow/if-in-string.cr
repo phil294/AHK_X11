ef88514f7dcc401b8f7ac122056574329a30dcc6
phil294 <philip@waritschlager.de>
2022-08-30 18:18:18 +0200

    add `IfInString` / `IfNotInString`

---
+++ src/cmd/control-flow/if-in-string.cr
@@ -0,0 +1,10 @@
+class Cmd::ControlFlow::IfInString < Cmd::Base
+	def self.min_args; 2 end
+	def self.max_args; 2 end
+	def self.multi_command; true end
+	def self.conditional; true end
+	def run(thread, args)
+		var_name, search_string = args
+		thread.get_var(var_name).includes?(search_string)
+	end
+end
