40c0575b0c894c3aae9989027e44cb8ad93e632a
phil294 <philip@waritschlager.de>
2022-11-12 22:03:59 +0100

    implement OutputVarControl option of MouseGetPos

--- src/cmd/x11/mouse/mouse-get-pos.cr
+++ src/cmd/x11/mouse/mouse-get-pos.cr
@@ -2,9 +2,23 @@ class Cmd::X11::Mouse::MouseGetPos < Cmd::Base
 	def self.min_args; 0 end
 	def self.max_args; 4 end
 	def run(thread, args)
+		# TODO should be win relative as per docs but is screen (also at_spi needs screen)
 		x, y, _, window = thread.runner.x_do.mouse_location
 		thread.runner.set_user_var(args[0], x.to_s) if args[0]? && ! args[0].empty?
 		thread.runner.set_user_var(args[1], y.to_s) if args[1]? && ! args[1].empty?
 		thread.runner.set_user_var(args[2], window.window.to_s) if args[2]? && ! args[2].empty?
+		if args[3]? && ! args[3].empty?
+			frame = thread.runner.at_spi.find_window(pid: window.pid, window_name: window.name)
+			if frame
+				_, class_NN = thread.runner.at_spi.find_descendant(frame, x: x, y: y)
+				if class_NN
+					thread.runner.set_user_var(args[3], class_NN)
+				else
+					thread.runner.set_user_var(args[3], "")
+				end
+			else
+				thread.runner.set_user_var(args[3], "")
+			end
+		end
 	end
 end
