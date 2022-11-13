7299ab149254039f1b092982262f0a88db0caba1
phil294 <philip@waritschlager.de>
2022-11-13 01:17:24 +0100

    add WinGetClass
    https://github.com/phil294/x_do.cr/commit/94f755a4f18a9a9820e9738b371cbe6b49731926

---
+++ src/cmd/x11/window/win-get-class.cr
@@ -0,0 +1,15 @@
+require "./win-util"
+
+class Cmd::X11::Window::WinGetClass < Cmd::Base
+	def self.min_args; 1 end
+	def self.max_args; 5 end
+	def run(thread, args)
+		match_conditions = args[1..]? || [] of ::String
+		out_var = args[0]
+		Util.match(thread, match_conditions, empty_is_last_found: true, a_is_active: true) do |win|
+			thread.runner.set_user_var(out_var, win.class_name || "")
+			return
+		end
+		thread.runner.set_user_var(out_var, "")
+	end
+end
