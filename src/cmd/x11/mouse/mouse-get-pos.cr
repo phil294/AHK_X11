1b33063449692985168701403cb07b34a821f62a
phil294 <philip@waritschlager.de>
2022-11-13 22:29:49 +0100

    fix wrong control for MouseGetPos
    (since last change to CoordMode)

--- src/cmd/x11/mouse/mouse-get-pos.cr
+++ src/cmd/x11/mouse/mouse-get-pos.cr
@@ -3,12 +3,6 @@ class Cmd::X11::Mouse::MouseGetPos < Cmd::Base
 	def self.max_args; 4 end
 	def run(thread, args)
 		x, y, _, window = thread.runner.x_do.mouse_location
-		if thread.settings.coord_mode_mouse == ::Run::CoordMode::RELATIVE
-			x, y = Cmd::X11::Window::Util.coord_screen_to_relative(thread, x, y)
-		end
-		thread.runner.set_user_var(args[0], x.to_s) if args[0]? && ! args[0].empty?
-		thread.runner.set_user_var(args[1], y.to_s) if args[1]? && ! args[1].empty?
-		thread.runner.set_user_var(args[2], window.window.to_s) if args[2]? && ! args[2].empty?
 		if args[3]? && ! args[3].empty?
 			frame = thread.runner.at_spi.find_window(pid: window.pid, window_name: window.name)
 			if frame
@@ -22,5 +16,11 @@ class Cmd::X11::Mouse::MouseGetPos < Cmd::Base
 				thread.runner.set_user_var(args[3], "")
 			end
 		end
+		if thread.settings.coord_mode_mouse == ::Run::CoordMode::RELATIVE
+			x, y = Cmd::X11::Window::Util.coord_screen_to_relative(thread, x, y)
+		end
+		thread.runner.set_user_var(args[0], x.to_s) if args[0]? && ! args[0].empty?
+		thread.runner.set_user_var(args[1], y.to_s) if args[1]? && ! args[1].empty?
+		thread.runner.set_user_var(args[2], window.window.to_s) if args[2]? && ! args[2].empty?
 	end
 end
