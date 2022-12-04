b6977252e6db27afa0ecf6f1d964dd2ee9032463
phil294 <philip@waritschlager.de>
2022-12-04 16:41:29 +0100

    fix gui color: allow to specify only control color

--- src/cmd/gtk/gui/gui-color.cr
+++ src/cmd/gtk/gui/gui-color.cr
@@ -4,12 +4,12 @@ class Cmd::Gtk::Gui::GuiColor < Cmd::Base
 	def run(thread, args)
 		gui_id = args[0]
 		thread.runner.display.gui.gui(thread, gui_id) do |gui|
-			if args[1]?
+			if args[1]? && ! args[1].empty?
 				window_color = thread.runner.display.gui.parse_rgba(args[1])
 				gui.window.override_background_color(::Gtk::StateFlags::NORMAL, window_color)
 				gui.window_color = window_color
 			end
-			if args[2]?
+			if args[2]? && ! args[2].empty?
 				gui.control_color = thread.runner.display.gui.parse_rgba(args[2])
 			end
 		end
