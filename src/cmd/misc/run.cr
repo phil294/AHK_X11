8fd026cecc8e3acc3cd9a738a1d13449d7b4cc66
phil294 <philip@waritschlager.de>
2023-01-03 16:48:42 +0100

    Run: fix escaping params, disable shell features and prevent ahk args from spanning multiple parameters
    args are now split into params *before* ahk variable substitution.
    
    for example,
    ```ahk
    a = b" "c" > d "
    Run, cmd "%a%"
    ```
    would previously excute `cmd "b" "c" > d ""`. Now, it's `cmd "b\" \"c\" > d \""`, so everything is treated literally and shell features like `>` or `&` aren't available anymore. If you need these features, you need to invoke `bash -c '%your_ahk_variable_containing_bash_stuff%'` manually.

--- src/cmd/misc/run.cr
+++ src/cmd/misc/run.cr
@@ -3,14 +3,21 @@ class Cmd::Misc::Run < Cmd::Base
 	def self.max_args; 6 end
 	@wait = false
 	# returns either `false` on failure or `i64` exit code or pid, depending on *@wait*.
-	private def try_execute(line, chdir = nil, stdout = false, stderr = false)
-		cmd = Process.parse_arguments(line)[0]?
-		return false, "", "" if ! cmd || ! ::Process.find_executable(cmd)
+	private def try_execute(thread, line, chdir = nil, stdout = false, stderr = false)
+		args = Process.parse_arguments(line).map do |arg|
+			Util::AhkString.parse_string(arg, thread.runner.settings.escape_char) do |var_name_lookup|
+				thread.get_var(var_name_lookup)
+			end
+		end
+		return false, "", "" if ! args[0]? || ! ::Process.find_executable(args[0])
+		{% if ! flag?(:release) %}
+			puts "[debug] runcmd[#{thread.id}]: cmd=#{args[0]} args=#{args[1..]}"
+		{% end %}
 		chdir = nil if chdir && chdir.empty?
 		stdout_m = stdout ? IO::Memory.new : Process::Redirect::Close
 		stderr_m = stderr ? IO::Memory.new : Process::Redirect::Close
 		begin
-			p = Process.new(line, chdir: chdir, shell: true, output: stdout_m, error: stderr_m)
+			p = Process.new(args[0], args[1..], chdir: chdir, output: stdout_m, error: stderr_m)
 		rescue e : IO::Error
 			return false, "", ""
 		end
@@ -18,6 +25,7 @@ class Cmd::Misc::Run < Cmd::Base
 		return ret, stdout_m.to_s, stderr_m.to_s
 	end
 	def run(thread, args)
+		target_raw = @args[0]
 		target = args[0]
 		pwd = args[1]?
 		opt = thread.parse_word_options(args[2]? || "")
@@ -25,16 +33,20 @@ class Cmd::Misc::Run < Cmd::Base
 		output_stdout = args[4]?
 		output_stderr = args[5]?
 
-		if target.starts_with?("open ")
+		if target_raw.starts_with?("open ")
+			target_raw = target_raw[5..]
 			target = target[5..]
 			open = true
-		elsif target.starts_with?("edit ")
+		elsif target_raw.starts_with?("edit ")
+			target_raw = target_raw[5..]
 			target = target[5..]
 			edit = true
-		elsif target.starts_with?("explore ")
+		elsif target_raw.starts_with?("explore ")
+			target_raw = target_raw[8..]
 			target = target[8..]
 			edit = true
-		elsif target.starts_with?("print ")
+		elsif target_raw.starts_with?("print ")
+			target_raw = "lp " + target_raw[6..]
 			target = "lp " + target[6..]
 			open = true
 		else
@@ -55,7 +67,7 @@ class Cmd::Misc::Run < Cmd::Base
 		# 	end
 
 		if edit
-			success, stdout, stderr = try_execute("gtk-launch \"$(xdg-mime query default text/plain)\" '#{target}'", chdir: pwd, stdout: !!output_stdout, stderr: !!output_stderr)
+			success, stdout, stderr = try_execute(thread, "gtk-launch '#{`xdg-mime query default text/plain`.strip}' '#{target_raw}'", chdir: pwd, stdout: !!output_stdout, stderr: !!output_stderr)
 		end
 
 		thread.runner.display.gui.act do
@@ -70,12 +82,11 @@ class Cmd::Misc::Run < Cmd::Base
 		end
 		
 		if ! success && open
-			success, stdout, stderr = try_execute(target, chdir: pwd, stdout: !!output_stdout, stderr: !!output_stderr)
+			success, stdout, stderr = try_execute(thread, target_raw, chdir: pwd, stdout: !!output_stdout, stderr: !!output_stderr)
 		end
 
 		if ! success
-			path = ::File.expand_path(target)
-			success = try_execute("xdg-open " + path) && ""
+			success = try_execute(thread, "xdg-open " + target_raw) && ""
 		end
 
 		if success
