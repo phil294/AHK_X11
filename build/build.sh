3f3d2dc58817002283e57e009e5834b4eec8ac2b
phil294 <philip@waritschlager.de>
2023-07-02 17:24:59 +0200

    include libthai.so.0.3.1 in final build
    fixes #538
    ref https://github.com/AppImageCommunity/pkg2appimage/issues/538

--- build/build.sh
+++ build/build.sh
@@ -18,7 +18,9 @@ cd build
 rm -rf AppDir
 # export DEPLOY_GTK_VERSION=3 # is autodetected
 export LD_LIBRARY_PATH="$PWD"
-./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage --desktop-file ../assets/ahk_x11.desktop --icon-file ../assets/ahk_x11.png --executable ../bin/ahk_x11 --library ./libxdo.so.3 --plugin gtk
+export BINCACHE_BIN_TARGET_FOLDER='$HOME/.cache/ahk_x11/AppImage'
+# libthai: https://github.com/phil294/AHK_X11/issues/45 https://github.com/AppImageCommunity/pkg2appimage/issues/538
+./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage --desktop-file ../assets/ahk_x11.desktop --icon-file ../assets/ahk_x11.png --executable ../bin/ahk_x11 --library ./libxdo.so.3 --library /lib/x86_64-linux-gnu/libthai.so.0.3.1 --plugin gtk
 rm -rf AppDir
 
 bin_name=ahk_x11-"$version"-x86_64.AppImage
@@ -35,6 +37,7 @@ mv ahk_x11-x86_64.AppImage "$bin_name"
 #     ahk_x11 --compile script1.ahk && ./script1 --compile script2.ahk && ./script2
 # but no one will ever do that.
 cp "$bin_name" "$bin_name".pre_installer
-./"$bin_name" --compile ../src/installer.ahk tmp && mv tmp "$bin_name"
+./"$bin_name" --compile ../src/installer.ahk tmp
+mv tmp "$bin_name"
 
 echo "success!"
