fb0b06c29cd0167f4ee61f468617d2f037b70a2f
phil294 <philip@waritschlager.de>
2023-07-28 19:52:43 +0200

    fix libthai dependency
    fixes #45

--- build/build.sh
+++ build/build.sh
@@ -19,7 +19,10 @@ rm -rf AppDir
 export LD_LIBRARY_PATH="$PWD"
 export BINCACHE_BIN_TARGET_FOLDER='$HOME/.cache/ahk_x11/AppImage'
 # libthai: https://github.com/phil294/AHK_X11/issues/45 https://github.com/AppImageCommunity/pkg2appimage/issues/538
-./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage --desktop-file ../assets/ahk_x11.desktop --icon-file ../assets/ahk_x11.png --executable ../bin/ahk_x11 --library ./libxdo.so.3 --library /lib/x86_64-linux-gnu/libthai.so.0.3.1 --plugin gtk
+# Other libraries that are missing on docker debian and frolvlad/alpine-glibc but are on excludelist
+# https://github.com/AppImageCommunity/pkg2appimage/blob/master/excludelist)
+# ...but that haven't caused problems so far: libfontconfig.so.1, libfribidi.so.0, libharfbuzz.so.0, libgpg-error.so.0(only alpine)
+./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage --desktop-file ../assets/ahk_x11.desktop --icon-file ../assets/ahk_x11.png --executable ../bin/ahk_x11 --library ./libxdo.so.3 --library /lib/x86_64-linux-gnu/libthai.so.0 --plugin gtk
 rm -rf AppDir
 
 bin_name=ahk_x11-"$version"-x86_64.AppImage
